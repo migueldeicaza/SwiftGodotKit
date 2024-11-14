abed3c69dd4469f4565b116377bcc336e3070cb0
Miguel de Icaza <miguel@gnome.org>
2024-11-14 18:28:38 -0500

    Revert

--- Package.swift
+++ Package.swift
@@ -7,7 +7,6 @@ let package = Package(
     name: "SwiftGodotKit",
     platforms: [
         .macOS(.v13),
-        .iOS(.v15)
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
@@ -20,8 +19,8 @@ let package = Package(
         .executable(name: "TrivialSample", targets: ["TrivialSample"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/migueldeicaza/SwiftGodot", revision: "e34b14c4c80a6c752b7f766d164f9854f688134f")
-        //.package(path: "../SwiftGodot"),
+        //.package(url: "https://github.com/migueldeicaza/SwiftGodot", revision: "e34b14c4c80a6c752b7f766d164f9854f688134f")
+        .package(path: "../SwiftGodot"),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
@@ -62,7 +61,7 @@ let package = Package(
         ),
         .binaryTarget (
             name: "binary_libgodot",
-            url: "https://github.com/migueldeicaza/SwiftGodotKit/releases/download/4.3.5/libgodot.xcframework.zip",
+            url: "https://github.com/migueldeicaza/SwiftGodotKit/releases/download/4.3.4/libgodot.xcframework.zip",
             checksum: "865ea17ad3e20caab05b3beda35061f57143c4acf0e4ad2684ddafdcc6c4f199"
         ),
         .systemLibrary(
