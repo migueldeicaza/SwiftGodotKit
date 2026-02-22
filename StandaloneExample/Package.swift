5789d08a7071a8f92b2e05bb678d409744e96305
Miguel de Icaza <miguel@gnome.org>
2026-02-22 13:42:54 -0500

    Fix the standalone sample

--- StandaloneExample/Package.swift
+++ StandaloneExample/Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "StandaloneExample",
     platforms: [
-        .macOS(.v13)
+        .macOS(.v14)
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
@@ -15,8 +15,8 @@ let package = Package(
             targets: ["StandaloneExample"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/migueldeicaza/SwiftGodot", revision: "1359586f2332d500d30661158e062a0e9f97751c"),
-        .package(url: "https://github.com/migueldeicaza/SwiftGodotKit", revision: "a7f90ce4fa3e20fa95b57f1d95704445e5844d3d")
+        .package(url: "https://github.com/migueldeicaza/SwiftGodot", revision: "48112dd50fffe01f0af78e445a16991ecdc6bc94"),
+        .package(url: "https://github.com/migueldeicaza/SwiftGodotKit", revision: "d4205c6a27754f037291cce54030786916b8e79a")
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
--- StandaloneExample/Sources/StandaloneExample/StandaloneExample.swift
+++ StandaloneExample/Sources/StandaloneExample/StandaloneExample.swift
@@ -1,69 +1,87 @@
-//
-//  main.swift
-//  SimpleGodotApp
-//
-//  Created by Miguel de Icaza on 9/6/23.
-//
-
-import Foundation
-import SwiftGodotKit
+import SwiftUI
 import SwiftGodot
+import SwiftGodotKit
 
-
-func loadScene (scene: SceneTree) {
-    let rootNode = Node3D()
-    let camera = Camera3D ()
-    camera.current = true
-    camera.position = Vector3(x: 0, y: 0, z: 2)
-    
-    rootNode.addChild(node: camera)
-    
-    func makeCuteNode (_ pos: Vector3) -> Node {
-        let n = SpinningCube()
-        n.position = pos
-        return n
-    }
-    rootNode.addChild(node: makeCuteNode(Vector3(x: 1, y: 1, z: 1)))
-    rootNode.addChild(node: makeCuteNode(Vector3(x: -1, y: -1, z: -1)))
-    rootNode.addChild(node: makeCuteNode(Vector3(x: 0, y: 1, z: 1)))
-    scene.root?.addChild(node: rootNode)
-}
-
-
+@Godot
 class SpinningCube: Node3D {
-    required init (nativeHandle: UnsafeRawPointer) {
-        super.init (nativeHandle: nativeHandle)
-    }
-    
-    required init () {
-        super.init ()
+    public override func _ready() {
         let meshRender = MeshInstance3D()
         meshRender.mesh = BoxMesh()
         addChild(node: meshRender)
     }
-    
-    override func _input (event: InputEvent) {
-        guard event.isPressed () && !event.isEcho () else { return }
-        print ("SpinningCube: event: isPressed ")
-    }
-    
+
     public override func _process(delta: Double) {
         rotateY(angle: delta)
     }
 }
 
-func registerTypes (level: GDExtension.InitializationLevel) {
-    switch level {
-    case .scene:
-        register (type: SpinningCube.self)
-    default:
-        break
+private func loadScene(scene: SceneTree) {
+    let rootNodeName = StringName("__standalone_example_root__")
+    if scene.root?.findChild(pattern: rootNodeName.description, recursive: false, owned: false) != nil {
+        return
+    }
+
+    let rootNode = Node3D()
+    rootNode.name = rootNodeName
+
+    let camera = Camera3D()
+    camera.current = true
+    camera.position = Vector3(x: 0, y: 0, z: 2)
+    rootNode.addChild(node: camera)
+
+    func makeCube(_ pos: Vector3) -> Node {
+        let cube = SpinningCube()
+        cube.position = pos
+        return cube
+    }
+
+    rootNode.addChild(node: makeCube(Vector3(x: 1, y: 1, z: 1)))
+    rootNode.addChild(node: makeCube(Vector3(x: -1, y: -1, z: -1)))
+    rootNode.addChild(node: makeCube(Vector3(x: 0, y: 1, z: 1)))
+    scene.root?.addChild(node: rootNode)
+}
+
+struct ContentView: View {
+    #if os(macOS)
+    @State private var app = GodotApp(
+        packFile: "",
+        godotPackPath: Bundle.main.bundlePath,
+        displayDriver: ProcessInfo.processInfo.environment["GODOT_DISPLAY_DRIVER"] ?? "embedded"
+    )
+    #else
+    @State private var app = GodotApp(packFile: "main.pck", godotPackPath: Bundle.main.bundlePath)
+    #endif
+
+    init() {
+        initHookCb = { level in
+            if level == .scene {
+                register(type: SpinningCube.self)
+            }
+        }
+    }
+
+    var body: some View {
+        GodotAppView(
+            onReady: { _ in
+                app.runOnGodotThread(async: true) {
+                    guard let scene = Engine.getMainLoop() as? SceneTree else { return }
+                    loadScene(scene: scene)
+                }
+            }
+        )
+        .frame(minWidth: 800, minHeight: 600)
+        .environment(\.godotApp, app)
+        .onAppear {
+            app.start()
+        }
     }
 }
 
 @main
-class Startup {
-    static func main () {
-        runGodot(args: [], initHook: registerTypes, loadScene: loadScene, loadProjectSettings: { settings in })
+struct StandaloneExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
     }
 }
